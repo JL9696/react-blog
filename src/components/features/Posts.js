6c7e31eba2ad59c0c9b11db2c27be7c8fde08af0
Joanna Lew/javascript-blog.git <lewjoanna96@gmail.com>
2023-04-21 14:46:16 +0200

    add view single post

---
+++ src/components/features/Posts.js
@@ -0,0 +1,19 @@
+import { useSelector } from "react-redux";
+import { getAllPosts } from "../../redux/postsRedux";
+import { Row } from "react-bootstrap";
+import Post from "./Post";
+
+
+
+const Posts = props => {
+
+  const posts = useSelector(getAllPosts);
+
+	return (
+        <Row className="justify-content-between">
+        {posts.map(post => <Post key={post.id} {...post} />)}  
+    </Row>  
+	);
+};
+
+export default Posts;
--- src/components/pages/SinglePost.js
+++ src/components/pages/SinglePost.js
@@ -1,9 +1,20 @@
+import { useSelector } from "react-redux";
+import { Navigate, useParams } from "react-router-dom";
+import { getPostById } from "../../redux/postsRedux";
+
 const SinglePost = () => {
+
+  const { id } = useParams();
+  const postData = useSelector(state => getPostById(state, id));
+
+  if(!postData) return <Navigate to="/" />
     return (
-        <div>
-            <p>SinglePost</p>
-        </div>
+      <div>
+        <h2>{postData.title}</h2>
+        <p>Author: {postData.author}</p>
+        <p>{postData.content}</p>
+      </div>
     );
 };
 
-export default SinglePost;
+  export default SinglePost;
